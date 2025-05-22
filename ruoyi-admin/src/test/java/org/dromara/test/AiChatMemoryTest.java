a030a546ef78a2934e6b1eb19d6a8e4c842b2fb4
QEES <fariyajahan303@gmail.com>
2025-05-23 00:44:17 +0800

    test(ai): 增加 AI 聊天记忆单元测试- 添加参数化测试方法，测试不同 memoryId 和 userId 的聊天内容 - 新增测试查询方法，验证根据 userId 查询聊天记录的功能- 在 ChatMemoryDb 类中添加 memoryId 字段并更新 userId 字段注解

--- ruoyi-admin/src/test/java/org/dromara/test/AiChatMemoryTest.java
+++ ruoyi-admin/src/test/java/org/dromara/test/AiChatMemoryTest.java
@@ -3,8 +3,15 @@ package org.dromara.test;
 import jakarta.annotation.Resource;
 import org.dromara.system.domain.ChatMemoryDb;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import java.util.List;
+import java.util.stream.Stream;
 
 @SpringBootTest
 public class AiChatMemoryTest {
@@ -12,11 +19,29 @@ public class AiChatMemoryTest {
     @Resource
     private MongoTemplate mongoTemplate;
 
-    @Test
-    public void test() {
+    static Stream<Arguments> chatMemoryData() {
+        return Stream.of(
+            Arguments.of(1L, 1L, "来自 memoryId为1 的第1条内容"),
+            Arguments.of(1L, 1L, "来自 memoryId为1  的第2条内容"),
+            Arguments.of(2L, 1L, "来自 memoryId为2  的第1条内容"),
+            Arguments.of(2L, 1L, "来自 memoryId为2  的第2条内容")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("chatMemoryData")
+    public void test(Long memoryId, Long userId, String content) {
         ChatMemoryDb chatMemoryDb = new ChatMemoryDb();
-        chatMemoryDb.setUserId(2L);
-        chatMemoryDb.setContent("测试2");
+        chatMemoryDb.setUserId(userId);
+        chatMemoryDb.setContent(content);
+        chatMemoryDb.setMemoryId(memoryId);
         mongoTemplate.save(chatMemoryDb);
+    }
+
+    @Test
+    public void testfind(){
+        Query query = Query.query(Criteria.where("user_id").is(2L));
+        List<ChatMemoryDb> userId = mongoTemplate.find(query, ChatMemoryDb.class);
+        System.out.println(userId);
     }
 }
--- ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/domain/ChatMemoryDb.java
+++ ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/domain/ChatMemoryDb.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Data
 @Document("chat_memory")
@@ -11,6 +12,10 @@ public class ChatMemoryDb {
     @Id
     private ObjectId id;
 
+    @Field("memory_id")
+    private Long memoryId;
+
+    @Field("user_id")
     private Long userId;
 
     private String content;
